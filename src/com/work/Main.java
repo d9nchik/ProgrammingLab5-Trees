9dc448f1237220a943a908ea3722519fc7175bff
Nikita Sakun <NikitaSakun1@gmail.com>
2020-05-20 21:01:36 +0300

    Console work

--- src/com/work/Main.java
+++ src/com/work/Main.java
@@ -1,13 +1,10 @@
 package com.work;
-
 import java.io.File;
-
 public class Main {
 
     public static void main(String[] args) {
         final Node node = CSVReader.readCSV(new File("src/com/work/ukraine_poi.csv"));
-        //TODO: test ability of serializing structure
         assert node != null;
-        System.out.println(node.getValues(new SimpleCirclePoint(50.60659, 30.45436, 3000)));
+        System.out.println(node.getValues(new SimpleCirclePoint(Double.parseDouble(args[0]), Double.parseDouble(args[1]), Double.parseDouble(args[2]))));
     }
 }
